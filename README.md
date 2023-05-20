90f13db2e1aaca99f19e18b43a00651e6a935cd1
KlimentLagrangiewicz <KlimStaljev@yandex.ru>
2023-05-20 15:50:41 +0300

    Editing readme file and adding file with source code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Generating-data-set
-Script in Python for generating a data set from sklearn_datasets
+Command line script in Python for generating a primitive data set from sklearn_datasets...  
++ First argument of command line is type of data set:  
+  +  0 — two moons data set;  
+  +  1 — swiss roll data set;  
+  +  else (for example 2) — blobs data set.  
++ Second argument is number of points/objects/instance.  
+
+For blobs dataset you may also to determine number of properties/features and number of centrums/clusters/class. Respectively, that third and fourth arguments.  
+
+## WARNING:   
+For using script you must install sklearn package. For Debian-based OS you can install from bash:  
+`$ sudo apt install python3-sklearn` 
+
+## Example of usage:
+`$ git clone https://github.com/KlimentLagrangiewicz/Generating-data-set`  
+`$ cd Generating-data-set/`  
+`$ python3 file.py 2 60 5 2`  
+ 
---
+++ file.py
@@ -0,0 +1,54 @@
+from sklearn.datasets import make_blobs, make_swiss_roll, make_moons
+import sys
+
+def getStrMatrix(X):
+    res = ""
+    for a in X:
+        for elem in a:
+            res = res + str(f'{elem:.20f}') + " "
+        res = res + str("\n")
+    return  res
+
+def getStrArray(arr):
+    res = ""
+    for a in arr:
+        res = res + str(f'{a:.0f}') + "\n"        
+    return  res
+
+
+try:
+    if (int(len(sys.argv)) < 3):
+        print("Not enough parameters...\n")
+        sys.exit(1)
+
+    if int(sys.argv[1]) == 0:
+        x, y = make_moons(n_samples = int(sys.argv[2]))
+        with open('testMoons' + sys.argv[2] + '.txt', 'w') as f:
+            f.write(getStrMatrix(x)) 
+        with open('idealSplittingMoons' + sys.argv[2] + '.txt', 'w') as f:
+            f.write(getStrArray(y))   
+    elif int(sys.argv[1]) == 1:
+        x, y = make_swiss_roll(n_samples = int(sys.argv[2]))
+        with open('testSwissRoll' + sys.argv[2] + '.txt', 'w') as f:
+            f.write(getStrMatrix(x))   
+    else:
+        if (int(len(sys.argv)) > 4):  
+            x, y = make_blobs(n_samples = int(sys.argv[2]), centers = int(sys.argv[4]), n_features = int(sys.argv[3]), random_state = 0) 
+            with open('testBlobs' + sys.argv[2] + '.txt', 'w') as f:
+                f.write(getStrMatrix(x)) 
+            with open('idealBlobs' + sys.argv[2] + '.txt', 'w') as f:
+                f.write(getStrArray(y))   
+        elif (int(len(sys.argv)) > 3):
+            x, y = make_blobs(n_samples = int(sys.argv[2]), centers = 2, n_features = int(sys.argv[3]), random_state = 0) 
+            with open('testBlobs' + sys.argv[2] + '.txt', 'w') as f:
+                f.write(getStrMatrix(x)) 
+            with open('idealBlobs' + sys.argv[2] + '.txt', 'w') as f:
+                f.write(getStrArray(y)) 
+        else:
+            x, y = make_blobs(n_samples = int(sys.argv[2]), centers = 2, n_features = 2, random_state = 0) 
+            with open('testBlobs' + sys.argv[2] + '.txt', 'w') as f:
+                f.write(getStrMatrix(x)) 
+            with open('idealBlobs' + sys.argv[2] + '.txt', 'w') as f:
+                f.write(getStrArray(y)) 
+except:
+    print("Something went wrong...") 
