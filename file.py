21653e251b6365e729fbb659edfddc1c1c264a49
KlimentLagrangiewicz <KlimStaljev@yandex.ru>
2023-05-20 22:58:34 +0300

    adding more info in output file name

--- file.py
+++ file.py
@@ -20,7 +20,6 @@ try:
     if (int(len(sys.argv)) < 3):
         print("Not enough parameters...\n")
         sys.exit(1)
-
     if int(sys.argv[1]) == 0:
         x, y = make_moons(n_samples = int(sys.argv[2]))
         with open('testMoons' + sys.argv[2] + '.txt', 'w') as f:
@@ -34,21 +33,21 @@ try:
     else:
         if (int(len(sys.argv)) > 4):  
             x, y = make_blobs(n_samples = int(sys.argv[2]), centers = int(sys.argv[4]), n_features = int(sys.argv[3]), random_state = 0) 
-            with open('testBlobs' + sys.argv[2] + '.txt', 'w') as f:
+            with open('testBlobs' + sys.argv[2] + "_" + sys.argv[3] + "_" + sys.argv[4] + '.txt', 'w') as f:
                 f.write(getStrMatrix(x)) 
-            with open('idealBlobs' + sys.argv[2] + '.txt', 'w') as f:
+            with open('idealBlobs' + sys.argv[2] + "_" + sys.argv[3] + "_" + sys.argv[4] + '.txt', 'w') as f:
                 f.write(getStrArray(y))   
         elif (int(len(sys.argv)) > 3):
             x, y = make_blobs(n_samples = int(sys.argv[2]), centers = 2, n_features = int(sys.argv[3]), random_state = 0) 
-            with open('testBlobs' + sys.argv[2] + '.txt', 'w') as f:
+            with open('testBlobs' + sys.argv[2] + "_" + sys.argv[3] + "_2" + '.txt', 'w') as f:
                 f.write(getStrMatrix(x)) 
-            with open('idealBlobs' + sys.argv[2] + '.txt', 'w') as f:
+            with open('idealBlobs' + sys.argv[2] + "_" + sys.argv[3] + "_2" + '.txt', 'w') as f:
                 f.write(getStrArray(y)) 
         else:
             x, y = make_blobs(n_samples = int(sys.argv[2]), centers = 2, n_features = 2, random_state = 0) 
-            with open('testBlobs' + sys.argv[2] + '.txt', 'w') as f:
+            with open('testBlobs' + sys.argv[2] + "_2_2" + '.txt', 'w') as f:
                 f.write(getStrMatrix(x)) 
-            with open('idealBlobs' + sys.argv[2] + '.txt', 'w') as f:
+            with open('idealBlobs' + sys.argv[2] + "_2_2" + '.txt', 'w') as f:
                 f.write(getStrArray(y)) 
 except:
     print("Something went wrong...") 
